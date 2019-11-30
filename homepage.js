31db56fd9b510ae950c9248cda9eb75ac542b50b
manan <mrpandya2017@gmail.com>
2019-11-30 15:06:14 +0530

    first

---
+++ homepage.js
@@ -0,0 +1,7 @@
+function cleary()
+{
+   document.getElementById("subject").value = " ";
+   document.getElementById("name").value = " ";
+   document.getElementById("email").value = " ";
+   document.getElementById("message").value = " ";
+}
