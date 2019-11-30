cbf414fe5be71fc25075efe3eb6f4aa35aa622e7
manan <mrpandya2017@gmail.com>
2019-11-30 15:40:29 +0530

    hey

--- homepage.js
+++ homepage.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function cleary()
 {
    document.getElementById("subject").value = " ";
@@ -6,7 +5,6 @@ function cleary()
    document.getElementById("email").value = " ";
    document.getElementById("message").value = " ";
 }
-=======
 function cleary()
 {
    document.getElementById("subject").value = " ";
@@ -14,4 +12,3 @@ function cleary()
    document.getElementById("email").value = " ";
    document.getElementById("message").value = " ";
 }
->>>>>>> da4880a6e5d8d66e0ec8355fc8a0ca260aa08e1b
