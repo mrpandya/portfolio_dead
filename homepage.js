243666da288f84e4694dc118c5cbcfed6c37e4fa
manan <mrpandya2017@gmail.com>
2020-01-29 08:59:38 +0530

    background update

--- homepage.js
+++ homepage.js
@@ -1,14 +1,23 @@
 function cleary()
 {
+   var name=document.getElementById("name");
+   var email=document.getElementById("email");
+   var subject=document.getElementById("subject");
+   var message=document.getElementById("message");
    document.getElementById("subject").value = " ";
    document.getElementById("name").value = " ";
    document.getElementById("email").value = " ";
    document.getElementById("message").value = " ";
+
 }
-function cleary()
-{
-   document.getElementById("subject").value = " ";
-   document.getElementById("name").value = " ";
-   document.getElementById("email").value = " ";
-   document.getElementById("message").value = " ";
-}
+// function cleary()
+// {
+//    document.getElementById("subject").value = " ";
+//    document.getElementById("name").value = " ";
+//    document.getElementById("email").value = " ";
+//    document.getElementById("message").value = " ";
+// }
+// function send()
+// {
+
+// }
