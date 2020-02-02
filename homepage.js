6d92eeec53d9bae11403c19f86f6afd7b8e6a6ef
manan <mrpandya2017@gmail.com>
2020-02-02 05:40:46 +0530

    changes in footer and send button

--- homepage.js
+++ homepage.js
@@ -8,8 +8,11 @@ function cleary()
    document.getElementById("name").value = " ";
    document.getElementById("email").value = " ";
    document.getElementById("message").value = " ";
-
+   document.getElementById("sendbutton").style="background-color:#7CE227";
 }
+//correct ans
+//background-color:#7CE227;
+//
 // function cleary()
 // {
 //    document.getElementById("subject").value = " ";
